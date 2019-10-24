5d28e0ceaf7fe37bbacf18343d0f5f173ee8b256
Joelle Perez <perezjoe@oregonstate.edu>
2019-10-24 11:50:22 -0700

    directory for generating rooms

--- program2/perezjoe.buildrooms.c
+++ program2/perezjoe.buildrooms.c
@@ -1,8 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/stat.h>
 
 int main() {
 
-    printf("Hello, Joelle. I've been birthed.\n");
-    return 0;
+	// printf("Hello, Joelle. I've been birthed.\n");
+
+	int pid = getpid(); //process ID of rooms
+	char dir_name[300];	//director to rooms
+
+	sprintf(dir_name, "perezjoe.rooms.%d", pid); //makes name of directory
+	mkdir(dir_name, 0777);	//makes directory with all permissions
+
+	printf("...\n");
+	printf("Directory made.\n");
+
+	return 0;
 }
