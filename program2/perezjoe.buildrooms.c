7f4fc8ad371d7cb14fef917bf21b2af9a1ddefdf
Joelle Perez <perezjoe@oregonstate.edu>
2019-10-24 11:57:12 -0700

    comments fixed

--- program2/perezjoe.buildrooms.c
+++ program2/perezjoe.buildrooms.c
@@ -4,13 +4,13 @@
 
 int main() {
 
-	// printf("Hello, Joelle. I've been birthed.\n");
+	/*printf("Hello, Joelle. I've been birthed.\n");*/
 
-	int pid = getpid(); //process ID of rooms
-	char dir_name[300];	//director to rooms
+	int pid = getpid(); /*process ID of rooms*/
+	char dir_name[300];	/*director to rooms*/
 
-	sprintf(dir_name, "perezjoe.rooms.%d", pid); //makes name of directory
-	mkdir(dir_name, 0777);	//makes directory with all permissions
+	sprintf(dir_name, "perezjoe.rooms.%d", pid); /*makes name of directory*/
+	mkdir(dir_name, 0777);	/*makes directory with all permissions*/
 
 	printf("...\n");
 	printf("Directory made.\n");
