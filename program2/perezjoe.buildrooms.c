b682edbf7f5ec61206485a0a614744a53a7f0ffe
Joelle Perez <perezjoe@oregonstate.edu>
2019-10-24 20:09:26 -0700

    randomize origin array so that it's easier to connect to different types of structs

--- program2/perezjoe.buildrooms.c
+++ program2/perezjoe.buildrooms.c
@@ -3,7 +3,7 @@
 #include <sys/stat.h>
 #include <time.h>
 
-int randomize();
+void shuffle(char *arr[]);
 
 int main() {
 	srand(time(NULL));
@@ -18,39 +18,35 @@ int main() {
 	sprintf(dir_name, "perezjoe.rooms.%d", pid); /*makes name of directory; change to include room*/
 	mkdir(dir_name, 0777);	/*makes directory with all permissions*/
 
-	randomize(room_names);
+	shuffle(room_names);
 
-	/*for (i = 0; i < 7; i++) {
+	/*prints names of randomly generated names*/
+	for (i = 0; i < 10; i++) {
+		printf ("%s\n", room_names[i]);
+	}
+
+	/*writes in each file*/
+	for (i = 0; i < 7; i++) {
 		sprintf(dir_path, "perezjoe.rooms.%d/%s", pid, room_names[i]); /*adds file name to dir path*/
 
-		/*fp = fopen (dir_path, "w"); /*opens file*/
-		/*fprintf (fp, "File %d", i); /*write into file*/
-		/*fclose(fp); /*close file*/
-	/*}*/
+		fp = fopen (dir_path, "w"); /*opens file*/
+		fprintf (fp, "File %d", i); /*write into file*/
+		fclose(fp); /*close file*/
+	}
 
 	return 0;
 }
 
-int randomize() {
-	int i = rand() % 9, j = rand() % 9;
+/*does not need a reference, passing in a pointer gets dereferenced so the original array was changed*/
+void shuffle (char *arr[]) {
+	int i, rand_num;
+	char *temp;
 
-	if(i != j) {
-		return i;
+	for (i = 0; i < 10; i++) {
+		rand_num = (rand() % (10-i)) + i;
+
+		temp = arr[i];
+		arr[i] = arr[rand_num];
+		arr[rand_num] = temp;
 	}
-
-	else {
-		i = rand() % 9;
-	}
- 
-	/*int i = rand() % 9, j = rand() % 9;
-	char *temp = arr[i];
-
-	arr[i] = arr[j];
-	arr[j] = temp;
-
-	printf("temp: %s\n", temp);
-	printf("%s\n", arr[i]);
-	printf("%s\n", arr[j]);*/
-	/*printf("%d\n", i);*/
-	/*printf("%d\n", j);*/
 }
