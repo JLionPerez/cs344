78a41b3fff5549e7e237ba1a0a4e04b790a66ba3
Joelle Perez <perezjoe@oregonstate.edu>
2019-10-24 20:52:00 -0700

    organized code, proceeding to structs

--- program2/perezjoe.buildrooms.c
+++ program2/perezjoe.buildrooms.c
@@ -3,38 +3,44 @@
 #include <sys/stat.h>
 #include <time.h>
 
+void make_dir_files(char *arr[]);
 void shuffle(char *arr[]);
 
 int main() {
 	srand(time(NULL));
 
-	FILE * fp;
-	int pid = getpid(); /*process ID of rooms*/
-	char dir_name[300];	/*directory to rooms*/
-	char dir_path[300];
 	char *room_names[] = {"Bentley", "Lacey", "Kookie", "Coco", "Eggplant", "Mango", "Joe", "Elle", "Tabs", "Alien"};
-	int i;
-
-	sprintf(dir_name, "perezjoe.rooms.%d", pid); /*makes name of directory; change to include room*/
-	mkdir(dir_name, 0777);	/*makes directory with all permissions*/
 
 	shuffle(room_names);
 
-	/*prints names of randomly generated names*/
+	/*temporary, prints names of randomly generated names*/
+	int i;
 	for (i = 0; i < 10; i++) {
 		printf ("%s\n", room_names[i]);
 	}
 
-	/*writes in each file*/
-	for (i = 0; i < 7; i++) {
-		sprintf(dir_path, "perezjoe.rooms.%d/%s", pid, room_names[i]); /*adds file name to dir path*/
-
-		fp = fopen (dir_path, "w"); /*opens file*/
-		fprintf (fp, "File %d", i); /*write into file*/
-		fclose(fp); /*close file*/
-	}
+	make_dir_files(room_names);
 
 	return 0;
+}
+
+void make_dir_files(char *arr[]) {
+	FILE * fp;
+	int i;
+	int pid = getpid(); /*process ID of rooms*/
+	char dir_name[300];	/*directory to rooms*/
+	char dir_path[300];
+
+	sprintf(dir_name, "perezjoe.rooms.%d", pid); /*makes name of directory; change to include room*/
+	mkdir(dir_name, 0777);	/*makes directory with all permissions*/
+
+	/*creates each file*/
+	for (i = 0; i < 7; i++) {
+		sprintf(dir_path, "perezjoe.rooms.%d/%s", pid, arr[i]); /*adds file name to dir path*/
+
+		fp = fopen (dir_path, "w"); /*opens file*/
+		fclose(fp); /*close file*/
+	}
 }
 
 /*does not need a reference, passing in a pointer gets dereferenced so the original array was changed*/
