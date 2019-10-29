5961f3b887d87e57bf16e9e57222a12ff7793589
Joelle Perez <perezjoe@oregonstate.edu>
2019-10-29 12:30:31 -0700

    new plan, maybe use a file to save recent dir path

--- program2/perezjoe.adventure.c
+++ program2/perezjoe.adventure.c
@@ -1,8 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/stat.h>
-/*#include <*/
+#include <sys/types.h>
+#include <time.h>
+#include <dirent.h>
+#include <stdbool.h>
+
+/*struct room {
+	char *name;
+	char *room_type;
+	int num_connections;
+	int room_id;
+	int room_connections[7];
+};*/
 
 int main() {
+    DIR *dir;
+    struct dirent *ent;
 
+    /*function to get current directory (because they have different directories in server)*/
+
+    if((dir = opendir (""))
+    /*char *dir_path = "perezjoe.rooms. *";
+    struct stat buf;
+    stat(dir_path, &buf);
+
+    int i;
+    for(i = 0; i < 2; i++) {
+        printf("File name: %s\n", dir_path);
+        printf("Latest modified time: %s", ctime(&buf.st_mtime));
+    }
+
+    return 0;*/
 }
