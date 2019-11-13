3e94951f8464e83a3e27983daaa23a677757fa34
Joelle Perez <perezjoe@oregonstate.edu>
2019-11-12 23:44:21 -0800

    reads input and prints out written

--- program3/smallsh.c
+++ program3/smallsh.c
@@ -2,9 +2,38 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-// GOAL: User Input
+// GOAL: Prompt
 // NEXT GOAL: Built-In Commands
-void main() {
 
+//prototypes
+void shell_loop();
+
+int main(int argc, char **argv) {
+
+    shell_loop();
+
+    return EXIT_SUCCESS;
+}
+
+void shell_loop() {
+    char *line = NULL;
+    char **args;
+    ssize_t bufsize = 0;
+    bool stat = true;
+
+    do {
+        printf(": ");
+        //read line input
+        getline(&line, &bufsize, stdin);
+        //split line input
+        //execute command
+
+        printf("Repeated: %s", line);
+
+        // free(line);
+        // free(args);
+        stat = false; 
+    } while(stat); //when stat is false it breaks otherwise keeps going
 }
