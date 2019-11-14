553d1b724442e202ee1eb4b4c8ec28815b175c68
Joelle Perez <perezjoe@oregonstate.edu>
2019-11-14 13:00:43 -0800

    testing

--- program3/smallsh.c
+++ program3/smallsh.c
@@ -25,81 +25,81 @@ void commands(char *input) {
     
 }
 
-void rep_pid(char *input) {
-    char strA[300];
-    char strB[300];
-    bool flag = true;
-    bool switched = true;
-    char *dd;
+void rep_pid(char *input) { //only gets $$ once or else inifite loop
+    char new_str[300];
+    char *dd = strstr(input, "$$");
 
-    // if(dd != NULL) {
-    //     flag = true;
-    // } else {
-    //     flag = false;
-    // }
-    
-    char* line = input;
-    printf("Line is input\n");
-    int pid = getpid();
-
-    while(flag) {
-
-        dd = strstr(line, "$$");
-        
-        if(dd == NULL) {
-            printf("Flag is false\n");
-            flag = false;
-            break;
-        }
-
-        printf("Flag is true\n");
-
+    if(dd != NULL) {
         dd[0] = '%';
         dd[1] = 'd';
-
-        if(switched) {
-            printf("Line is A\n");
-            line = strA;
-            switched = false;
-        } else {
-            printf("Line is B\n");
-            line = strB;
-            switched = true;
-        }
-
-        sprintf(line, input, pid);
-        printf("%s\n", line);
+        sprintf(new_str, input, getpid());
+        printf("%s\n", new_str);
     }
+    // char strA[300];
+    // char strB[300];
+    // bool flag = true;
+    // bool switched = true;
+    // char *dd;
+    
+    // char* line = input;
+    // printf("Line is input\n");
+    // int pid = getpid();
+
+    // while(flag) {
+
+    //     dd = strstr(line, "$$");
+        
+    //     if(dd == NULL) {
+    //         printf("Flag is false\n");
+    //         flag = false;
+    //         break;
+    //     }
+
+    //     printf("Flag is true\n");
+
+    //     dd[0] = '%';
+    //     dd[1] = 'd';
+
+    //     if(switched) {
+    //         printf("Line is A\n");
+    //         line = strA;
+    //         switched = false;
+    //     } else {
+    //         printf("Line is B\n");
+    //         line = strB;
+    //         switched = true;
+    //     }
+
+    //     sprintf(line, input, pid);
+    //     printf("%s\n", line);
+    // }
 }
 
 void shell_loop(char *input) {
-    //char **args;
+    char **args; //holds arguments
     ssize_t bufsize = 0; //initial 0
     bool stat = true;
+    int i = 0;
 
     do {
         printf(": ");
         getline(&input, &bufsize, stdin);
         strtok(input, "\n"); //removes enter
 
-        rep_pid(input);
+        if (strlen(input) > 2048) {
+            printf("error too many characters\n");
+        }
 
-        // char *dd = strstr(input, "$$"); //gets pointer of where $$ is, ex. dd[0] = '%'dd[1] = 'd' would replace string with 
-        // dd[0] = '%';
-        // dd[1] = 'd';
-        // sprintf(new_str, input, getpid());
+        while() { //goal: parse in arguments
+            strtok(input, " ");
+            //i++;
+        }
 
-        // size_t i = 0;
-        // while(new_str[i] != '\0') {
-        //     char *dds = strstr(new_str, "$$"); //gets pointer of where $$ is, ex. dd[0] = '%'dd[1] = 'd' would replace string with 
-        //     dds[0] = '%';
-        //     dds[1] = 'd';
-        //     sprintf(input, new_str, getpid());
+        printf("%s\n", input);
 
-        //     i++;
-        // }
-        // printf("%s\n", new_str);
+        //rep_pid(input);
+        fflush(stdout);
 
-        stat = false; //temporary
+        //stat = false; //temporary
     } while(stat); //when stat is false it breaks otherwise keeps going
 }
