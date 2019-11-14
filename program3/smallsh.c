f097d393940f5a9fc1c883d292dceaf04c5f7dc9
Joelle Perez <perezjoe@oregonstate.edu>
2019-11-13 18:05:46 -0800

    switch

--- program3/smallsh.c
+++ program3/smallsh.c
@@ -11,6 +11,7 @@
 //prototypes
 void shell_loop(char *input);
 void commands(char *input);
+void rep_pid(char *input);
 
 int main() {
     char *line;
@@ -20,40 +21,68 @@ int main() {
 }
 
 void commands(char *input) {
+    
+}
 
+void rep_pid(char *input) {
+    char strA[300];
+    char strB[300];
+    bool flag = true;
+    bool switch;
+    char *dd;
+
+    // if(dd != NULL) {
+    //     flag = true;
+    // } else {
+    //     flag = false;
+    // }
+    char* line = input;
+    int pid = getpid();
+
+
+    while(flag) {
+        dd = strstr(line, "$$");
+        
+        if(dd == NULL) {
+            flag = false;
+            break;
+        }
+
+        dd[0] = '%';
+        dd[1] = 'd';
+
+        sprintf(new_str, input, pid);
+    }
+    printf("%s\n", new_str);
 }
 
 void shell_loop(char *input) {
     //char **args;
-    char new_str[100];
     ssize_t bufsize = 0; //initial 0
     bool stat = true;
 
     do {
-
-        // if (strcmp(line, "exit") == 0) {
-        //     stat = false;
-        // }
-
         printf(": ");
         getline(&input, &bufsize, stdin);
-        strtok(input, "\n");
+        strtok(input, "\n"); //removes enter
 
-        char *dd = strstr(input, "$$"); //gets pointer of where $$ is, ex. dd[0] = '%'dd[1] = 'd' would replace string with 
-        dd[0] = '%';
-        dd[1] = 'd';
-        sprintf(new_str, input, getpid());
+        rep_pid(input);
 
-        printf("%s", new_str);
-        //sprintf(input, "%d", );
+        // char *dd = strstr(input, "$$"); //gets pointer of where $$ is, ex. dd[0] = '%'dd[1] = 'd' would replace string with 
+        // dd[0] = '%';
+        // dd[1] = 'd';
+        // sprintf(new_str, input, getpid());
 
         // size_t i = 0;
-        // while(input[i] != '\0') {
+        // while(new_str[i] != '\0') {
+        //     char *dds = strstr(new_str, "$$"); //gets pointer of where $$ is, ex. dd[0] = '%'dd[1] = 'd' would replace string with 
+        //     dds[0] = '%';
+        //     dds[1] = 'd';
+        //     sprintf(input, new_str, getpid());
 
-        //     //printf("%c\n", input[i]);
         //     i++;
         // }
-        //printf("Repeated: %s", input);
+        // printf("%s\n", new_str);
 
         stat = false; //temporary
     } while(stat); //when stat is false it breaks otherwise keeps going
