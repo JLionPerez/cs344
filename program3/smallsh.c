7f4a46f36fc3b7b75b2e20f5851e0894ab13fe4a
Joelle Perez <perezjoe@oregonstate.edu>
2019-11-13 16:04:39 -0800

    dollar dollar signs change into pids

--- program3/smallsh.c
+++ program3/smallsh.c
@@ -3,37 +3,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 // GOAL: Prompt
 // NEXT GOAL: Built-In Commands
 
 //prototypes
-void shell_loop();
+void shell_loop(char *input);
+void commands(char *input);
 
-int main(int argc, char **argv) {
+int main() {
+    char *line;
+    shell_loop(line); //get user input
 
-    shell_loop();
-
-    return EXIT_SUCCESS;
+    return 0;
 }
 
-void shell_loop() {
-    char *line = NULL;
-    char **args;
-    ssize_t bufsize = 0;
+void commands(char *input) {
+
+}
+
+void shell_loop(char *input) {
+    //char **args;
+    char new_str[100];
+    ssize_t bufsize = 0; //initial 0
     bool stat = true;
 
     do {
+
+        // if (strcmp(line, "exit") == 0) {
+        //     stat = false;
+        // }
+
         printf(": ");
-        //read line input
-        getline(&line, &bufsize, stdin);
-        //split line input
-        //execute command
+        getline(&input, &bufsize, stdin);
+        strtok(input, "\n");
 
-        printf("Repeated: %s", line);
+        char *dd = strstr(input, "$$"); //gets pointer of where $$ is, ex. dd[0] = '%'dd[1] = 'd' would replace string with 
+        dd[0] = '%';
+        dd[1] = 'd';
+        sprintf(new_str, input, getpid());
 
-        // free(line);
-        // free(args);
-        stat = false; 
+        printf("%s", new_str);
+        //sprintf(input, "%d", );
+
+        // size_t i = 0;
+        // while(input[i] != '\0') {
+
+        //     //printf("%c\n", input[i]);
+        //     i++;
+        // }
+        //printf("Repeated: %s", input);
+
+        stat = false; //temporary
     } while(stat); //when stat is false it breaks otherwise keeps going
 }
